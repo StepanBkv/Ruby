bd6f34e607c8b074190641e6ccf3909048e8558e
Степан Бабаков <stepan.babakov.1999@mail.ru>
2022-02-09 14:22:28 +0300

    Задание 1

--- First.rb
+++ First.rb
@@ -1,1 +1,37 @@
-
+#puts "Hello #{#ARGV[0]}"
+# 7 Variant
+# 1 Задание
+puts ("Hello world")
+# 2 Задание
+username = "Stepan"
+puts"Привет, #{username}!"
+#puts "Привет, #{ARGV[0]}!
+# 3 задание
+ printf("Какой твой любимый язык программирования, %s?\n", username)
+ lang = gets.chomp.strip
+ lang.capitalize!
+ case lang
+   when "Ruby"
+   puts "Подлиза)))))"
+   when "Python"
+   puts "Нормально, нормально. Но скоро будет Ruby"
+   when "C++"
+   puts "Жесть. Но скоро будет Ruby)"
+   when "R"
+   puts "Статист значит. Но скоро будет Ruby"
+   when "JS"
+   puts "Извращюга. Но Ruby лучше)"
+   when "C#"
+   puts "Скоро будет Ruby"
+   when "PHP"
+   puts "ъеъ. Skoro budet Руби"
+ else
+   puts "Такого языка нет("
+ end
+# 4 задание
+ puts "Введите команду на Ruby!"
+ input = gets.chomp.strip
+ eval input
+ puts "Введите командную строку своей ОС!"
+ input = gets.chomp.strip
+ system input
