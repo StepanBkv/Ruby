9b8517ec482593bbabf1ad67438a7b7a964d40f1
Степан Бабаков <stepan.babakov.1999@mail.ru>
2022-04-05 18:35:41 +0300

    5 блок 1(переделанный, так жедобавлен дополнительный метод поиска русских символов в тексте), 2, 3(сделанно с помощью метода scan)

--- Sixth.rb
+++ Sixth.rb
@@ -4,19 +4,23 @@
 # Так как если отдадим всю строку на проверку, регулярка не сработает
 # Надо спросить.
 def russian_char_size str
-  str.each_char {|char| char if /[а-яА-Я]/.match(char)}.size
+  #str.each_char {|char| char if /[а-яА-Я]/.match(char)}.size
+  str.scan(/[а-яА-Я]*/).inject { |sum = "", i| sum + i }.size
 end
 
-print russian_char_size str = "ааааaвввв"
+#print russian_char_size str = "ааааadsdsвввв"
 # 9. Дана строка. Необходимо проверить образуют ли строчные
 # символы латиницы палиндром.
 
 def polindrom str
-
+  str == str.reverse
 end
 
+#print polindrom str = "ada"
 # 18. Найти в тексте даты формата «день.месяц.год».
 
 def form_date str
-
+  str.scan(/([0-3][0-9]\.[0-1][0-2]\.[0-9]{4})/)
 end
+
+#print form_date str = "32.12.1231 fdd 12.12.4231"
