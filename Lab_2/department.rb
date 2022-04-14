a2e98cbdb9fc23d72fc6c6706fdd8c429fcf9cdc
Степан Бабаков <stepan.babakov.1999@mail.ru>
2022-04-14 13:09:39 +0300

    Серилиазация, десерилизация. Исправил немного to_s. Добавил yield и проверку не передан ли блок

--- Lab_2/department.rb
+++ Lab_2/department.rb
@@ -1,29 +1,33 @@
 class Department
   attr_accessor :name
-  attr_accessor :phone
-
+  attr_reader :phone
   @@count_object = 0
   @@departmnet_array = []
 
   def initialize name, phone
     @name = name
-    if valid_phone phone
-      @phone = phone
-    else
-      raise("Некорректный номер телефона!")
-    end
-
+    self.phone = phone
     @@count_object += 1
     @duty = []
     @@departmnet_array.push(self)
   end
 
+  def phone= (phone)
+    if valid_phone phone
+      @phone = phone
+    else
+      raise("Некорректный номер телефона!")
+    end
+  end
+
   def Department.get_count_object
     @@count_object
   end
+
   def Department.get_department_array
     @@departmnet_array
   end
+
   def duty= str
     @duty.push(str)
   end
@@ -35,7 +39,11 @@ class Department
   def deleted_duty_dedicated
     @duty[@duty_dedicated] = nil
     @duty.compact!
-    @duty_dedicated = nil
+    if !@duty.empty? and @duty_dedicated != 0
+      @duty_dedicated -= 1
+    else
+      @duty_dedicated == nil
+    end
   end
 
   def text_duty_dedicated
@@ -47,39 +55,64 @@ class Department
   end
 
   def show_duty_all
-    for i in @duty do
-      yield i
+    self.to_s(@duty) { |i| i + ", " }
+  end
+
+  def to_s string_array
+    sum_string = ""
+    for i in string_array
+      if block_given?
+        sum_string += yield i
+      else
+        sum_string += i
+      end
     end
+    sum_string
   end
 
   def show_data
-    [@name, @phone]
+    self.to_s([@name, @phone]){|i| i + " "}
   end
 
   def valid_phone phone
-    phone =~ /(\+7||8)[0-9]{10}/
+    (phone =~ /(8){1}[0-9]{10}/ and phone.size == 11) |
+      (phone =~ /\+7{1}[0-9]{10}/ and phone.size == 12) |
+      (phone =~ /[0-9]{10}/ and phone.size == 10)
   end
 
-  def Department.read_from_txt file_name
-    f = File.new(file_name)
-    lst = f.read.split("\n")
-    f.close
-    lst.map { |i| Department.new(i.split[0], i.split[1]) }
+  def Department.valid_phone phone
+    (phone =~ /(8){1}[0-9]{10}/ and phone.size == 11) |
+      (phone =~ /\+7{1}[0-9]{10}/ and phone.size == 12) |
+      (phone =~ /[0-9]{10}/ and phone.size == 10)
   end
 
+  def Department.phone=(phone)
+    if valid_phone phone
+      @phone = phone
+    else
+      raise("Некорректный номер телефона!")
+    end
+  end
 end
 
-#Person_1 = Department.new("Степан", "89002355541")
-# Person_2 = Department.new("Андрей", "89382425742")
-# Person_3 = Department.new("Виктор", "89286455841")
-# Person_3.duty = "Программист"
-# Person_3.duty = "DevOps"
-# Person_3.duty = "Аналитик"
-# Person_3.duty_dedicated = 0
-# Person_3.deleted_duty_dedicated
-# Person_3.show_duty_all { |i| puts i }
+def read_from_txt file_name
+  f = File.new(file_name)
+  lst = f.read.split("\n")
+  f.close
+  lst.map { |i| Department.new(i.split[0], i.split[1]) }
+end
 
-Department.read_from_txt "department_file"
-# print Department.get_department_array
-Department.get_department_array[0]
-Department.get_department_array[0].show_duty_all {|i| print i}
+def write_to_txt department_array, file_name
+  f = File.open(file_name, "w+")
+  for i in department_array
+    f.write("#{i.show_data} #{i.show_duty_all} \n")
+  end
+  f.close
+end
+
+Department_array = read_from_txt "department_file"
+Department_array.push(Department.new("Andrew", "89324563124"))
+Department_array[0].duty = "Программист"
+Department_array[0].duty = "Jhfhfn"
+print Department_array[0].show_data
+write_to_txt(Department_array, "department_file")
