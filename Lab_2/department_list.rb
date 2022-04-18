8d76eefc3ba69e5163d8296e2d5783026d6730f5
Степан Бабаков <stepan.babakov.1999@mail.ru>
2022-04-18 21:19:26 +0300

    Добавлен sort, size

--- Lab_2/department_list.rb
+++ Lab_2/department_list.rb
@@ -2,6 +2,7 @@ require './Lab_2/department.rb'
 
 class Department_list < Department
   @choose_note = nil
+  @size = 0
 
   def initialize department_array
     @department_array = department_array
@@ -34,7 +35,28 @@ class Department_list < Department
   def delete_note
     @department_array[@choose_note] = nil
     @department_array.compact!
+    #дописать недопускание выкдывания exseption
     self.choose_note = @choose_note - 1
+  end
+
+  def size
+    size = 0
+    for i in @department_array
+      size += 1
+    end
+    size
+  end
+
+  def sort
+    pred = @department_array[0]
+    for i in (1..@department_array.size - 1)
+      for j in (1..@department_array.size - 1)
+        if pred.name > @department_array[j].name
+          @department_array[j - 1], @department_array[j] = @department_array[j], @department_array[j - 1]
+        end
+        pred = @department_array[j]
+      end
+    end
   end
 
   def Department_list.read_from_YAML file_name
@@ -42,7 +64,7 @@ class Department_list < Department
   end
 
   def Department_list.read_from_txt file_name
-    @departmnet_array = super file_name
+    Departmnet_list.new(super file_name)
   end
 
   def Department_list.write_to_txt file_name, department_array
@@ -71,6 +93,9 @@ end
 department_list = Department_list.read_from_YAML "./Lab_2/department_file.yaml"
 department_list.add_note("HR", "89002333321", "Старший Hr")
 
+department_list.each { |i| puts i }
+department_list.sort
+print "\n"
 department_list.each { |i| puts i }
 
 
